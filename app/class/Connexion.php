34c1cc1941ccc5643b96133061d6deb498c83c94
lakroune <lakroune00@gmail.com>
2025-12-31 15:34:40 +0100

    bug : correger la bug de connexion

--- app/class/Connexion.php
+++ app/class/Connexion.php
@@ -2,19 +2,28 @@
 
 class Connexion
 {
-    private $nomDB = "maBagnole";
-    private $userDB = "root";
-    private $passDB = "";
-    private $hostDB = "localhost";
-    public function __construct() {}
-    public function connect()
+    private string $nomDB;
+    private string $userDB;
+    private string $passDB;
+    private string $hostDB;
+    // constructeur private pour eviter l'instanciation de la class
+    private function __construct()
     {
+        $this->nomDB = "MaBagnole";
+        $this->userDB = "root";
+        $this->passDB = "";
+        $this->hostDB = "localhost";
+    }
+    public static function connect()
+    {
+        $connexion = new Connexion();
         try {
-            $pdo = new PDO("mysql:dbname=$this->nomDB;host=$this->hostDB", $this->userDB, $this->passDB);
+            $pdo = new PDO("mysql:dbname=$connexion->nomDB;host=$connexion->hostDB", $connexion->userDB, $connexion->passDB);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         } catch (PDOException $e) {
-          
+            error_log(date('y-m-d h:i:s') . " Connexion :error ." . $e . PHP_EOL, 3, "error.log");
+            return null;
         }
     }
 }
