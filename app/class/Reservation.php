c718a202363bddf65ccb395dbe7dc88b629db576
lakroune <lakroune00@gmail.com>
2025-12-31 14:41:36 +0100

    feat : initialise la class Reservation

---
+++ app/class/Reservation.php
@@ -0,0 +1,32 @@
+ <?php
+
+class Reservation
+{
+    private int $idReservation;
+    private DateTime $dateReservation;
+    private DateTime $dateDebutReservation;
+    private DateTime $dateFinReservation;
+    private string $lieuChange;
+    private int $idVehicule;
+    private string $statusReservation;
+    private int $idClient;
+    // constructeur
+    public function __construct() {}
+    // getters
+    public function __get($aturibut) {}
+    // setters
+    public function __set($aturibut, $value) {}
+    public function __toString() {}
+    // ajouter Reservation
+    public function ajouterReservation() {}
+    // confirmer Reservation
+    public function confirmerReservation(int $idReservation) {}
+    // annuler Reservation
+    public function annulerReservation(int $idReservation) {}
+    //get Reservation
+    public function getReservation(int $idReservation) {}
+    //getAll Reservations
+    public static function getAllReservations() {}
+    //conter Reservations
+    public static function conterReservations() {}
+}
