c0686c9dcdefdab755f21500f84f51bb975af4a8
lakroune <lakroune00@gmail.com>
2025-12-31 14:10:39 +0100

    feat : initialise la class Utilisateur

--- app/class/Utilisateur.php
+++ app/class/Utilisateur.php
@@ -7,6 +7,26 @@ class Utilisateur
     public string $email;
     public string $paword;
     public string $role;
+    // constructeur
     public function __construct() {}
+    // getters
+    public function __get($aturibut)
+    {
+        return $this->$aturibut;
+    }
+    // setters
+    public function __set($aturibut, $value)
+    {
+        $this->$aturibut = $value;
+    }
+    //toString
+    public function __toString()
+    {
+        return "idUtilisateur=$this->idUtilisateur, nomUtilisateur=$this->nomUtilisateur, prenomUtilisateur=$this->prenomUtilisateur, email=$this->email, role=$this->role";
+    }
+    //seconnecter
+    public function seconnecter() {}
+    //sdeconnecter
+    public function sdeconnecter() {}
     
 }
