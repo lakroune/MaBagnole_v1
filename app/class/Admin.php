15b5c577dd3993e0c75f99cad03b9f80c3ddc7c2
lakroune <lakroune00@gmail.com>
2025-12-31 14:14:41 +0100

    feat : initialise la class Admin

--- app/class/Admin.php
+++ app/class/Admin.php
@@ -3,8 +3,14 @@
 class Admin extends Utilisateur
 
 {
+    // constructeur
     public function __construct()
     {
         parent::__construct();
+    }
+    //tostring
+    public function __toString()
+    {
+        return parent::__toString() ;
     }
 }
