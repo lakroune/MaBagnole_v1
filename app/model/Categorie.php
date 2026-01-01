1be5459b2beee45c627fda16022e69a0ae231164
lakroune <lakroune00@gmail.com>
2026-01-01 13:17:56 +0100

    feat: fais la crud de class Categorie

--- app/model/Categorie.php
+++ app/model/Categorie.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace app\model;
+
+use app\model\Connexion;
 
 class Categorie
 {
@@ -30,9 +33,71 @@ class Categorie
     {
         return 0;
     }
-    public function AjouterCategorie() {}
-    public function ModifierCategorie() {}
-    public function SupprimerCategorie() {}
-    public function RechercherCategorie(int $idCategorie) {}
+    public function AjouterCategorie(): bool
+    {
+        try {
+            $db = Connexion::connect()->getConnexion();
+            $sql = "insert into categories (titreCategorie,descriptionCategorie) values (:titreCategorie,:descriptionCategorie)";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":titreCategorie", $this->titreCategorie);
+            $stmt->bindParam(":descriptionCategorie", $this->descriptionCategorie);
+            if ($stmt->execute())
+                return true;
+            else
+                return false;
+        } catch (\Exception $e) {
+            error_log(date('y-m-d h:i:s') . " Connexion :error ." . $e . PHP_EOL, 3, "error.log");
+            return false;
+        }
+    }
+    public function ModifierCategorie(): bool
+    {
+        try {
+            $db = Connexion::connect()->getConnexion();
+            $sql = "update categories set titreCategorie=:titreCategorie, descriptionCategorie=:descriptionCategorie where idCategorie=:idCategorie";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":titreCategorie", $this->titreCategorie);
+            $stmt->bindParam(":descriptionCategorie", $this->descriptionCategorie);
+            $stmt->bindParam(":idCategorie", $this->idCategorie);
+            if ($stmt->execute())
+                return true;
+            else
+                return false;
+        } catch (\Exception $e) {
+            error_log(date('y-m-d h:i:s') . " Connexion :error ." . $e . PHP_EOL, 3, "error.log");
+            return false;
+        }
+    }
+    public function SupprimerCategorie(int $idCategorie): bool
+    {
+        try {
+            $db = Connexion::connect()->getConnexion();
+            $sql = "delete from categories where idCategorie=:idCategorie";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":idCategorie", $idCategorie);
+            if ($stmt->execute())
+                return true;
+            else
+                return false;
+        } catch (\Exception $e) {
+            error_log(date('y-m-d h:i:s') . " Connexion :error ." . $e . PHP_EOL, 3, "error.log");
+            return false;
+        }
+    }
+    public function RechercherCategorie(int $idCategorie): ?Categorie
+    {
+        try {
+            $db = Connexion::connect()->getConnexion();
+            $sql = "select * from categories where idCategorie=:idCategorie";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":idCategorie", $idCategorie);
+            $stmt->execute();
+            $categorie = $stmt->fetch(\PDO::FETCH_OBJ);
+            return $categorie;
+        } catch (\Exception $e) {
+            error_log(date('y-m-d h:i:s') . " Connexion :error ." . $e . PHP_EOL, 3, "error.log");
+            return null;
+        }
+    }
     public function AfficherCategorie(int $idCategorie) {}
 }
